20ba61d65b5f1e0814bb512fba1cd2ad7e8b8208
Stéphane SALLES <s.salles@sicavonline.fr>
2017-09-13 08:12:58 +0000

    :art: optimisation installation

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
+VIMRC_PATH="$HOME/.vimrc"
+
 git submodule init
 git submodule update
 
-cp -rf ./.vim ~
-cd ~
-rm .vimrc
-ln -s .vim/vimrc .vimrc
+cp -rf ./.vim $HOME
+
+if [ -f "$VIMRC_PATH" ]; then
+  rm $VIMRC_PATH
+fi
+
+ln -s $HOME/.vim/vimrc $VIMRC_PATH
